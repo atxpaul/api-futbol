c7fa4273764487c3cd9b5868ccd79604803f0296
atxpaul <pablo.pan.veira@gmail.com>
2021-11-18 18:36:24 +0100

    Added basic front for queries

---
+++ public/js/main.js
@@ -0,0 +1,85 @@
+const positive = document.querySelector('.positive');
+const negative = document.querySelector('.negative');
+const draw = document.querySelector('.draw');
+const error = document.querySelector('.error');
+
+function hideAllPanels() {
+  positive.classList.add('hidden');
+  negative.classList.add('hidden');
+  draw.classList.add('hidden');
+  error.classList.add('hidden');
+}
+
+function showPanel(panel) {
+  panel.classList.remove('hidden');
+}
+
+async function getData(url) {
+  const response = await fetch(url);
+  if (!response.ok) throw 'Error conseguindo o resultado do Dépor';
+  const data = await response.json();
+  return data;
+}
+
+function showError(message) {
+  hideAllPanels();
+  error.querySelector('p').innerText = message;
+  showPanel(error);
+}
+
+function showPositive(lastMatch, nextMatch) {
+  hideAllPanels();
+  showPanel(positive);
+  positive.querySelector(
+    'p.lastMatch'
+  ).innerHTML = `${lastMatch.result} contra o ${lastMatch.rival}`;
+  positive.querySelector('p.nextMatch').innerHTML = `⚽${nextMatch.rival}<br />
+  📆${nextMatch.day}<br />
+  🕑${nextMatch.hour}<br />
+  📍${nextMatch.riazor ? 'En #ANosaCasa' : 'A domicilio'}`;
+}
+//🕑📆📍⚽
+
+function showNegative(lastMatch, nextMatch) {
+  hideAllPanels();
+  showPanel(negative);
+  negative.querySelector(
+    'p.lastMatch'
+  ).innerHTML = `${lastMatch.result} contra o ${lastMatch.rival}`;
+  negative.querySelector('p.nextMatch').innerHTML = `⚽${nextMatch.rival}<br />
+  📆${nextMatch.day}<br />
+  🕑${nextMatch.hour}<br />
+  📍${nextMatch.riazor ? 'En #ANosaCasa' : 'A domicilio'}`;
+}
+
+function showDraw(lastMatch, nextMatch) {
+  hideAllPanels();
+  showPanel(draw);
+  draw.querySelector(
+    'p.lastMatch'
+  ).innerHTML = `${lastMatch.result} contra o ${lastMatch.rival}`;
+  draw.querySelector('p.nextMatch').innerHTML = `⚽${nextMatch.rival}<br />
+  📆${nextMatch.day}<br />
+  🕑${nextMatch.hour}<br />
+  📍${nextMatch.riazor ? 'En #ANosaCasa' : 'A domicilio'}`;
+}
+
+async function processData(matches) {
+  const lastMatch = matches[0];
+  const nextMatch = matches[1];
+  if (lastMatch.winner == 'Dépor') {
+    showPositive(lastMatch, nextMatch);
+  } else if (lastMatch.winner == 'Draw') {
+    showDraw(lastMatch, nextMatch);
+  } else {
+    showNegative(lastMatch, nextMatch);
+  }
+}
+
+async function main() {
+  const matches = await getData(`${document.URL}api/depor`);
+  await processData(matches);
+  console.log(matches);
+}
+
+main();
--- src/jobs/Scrap.js
+++ src/jobs/Scrap.js
@@ -15,7 +15,6 @@ class Scrap {
       const $ = cheerio.load(pageContent.data);
       let result;
 
-      //console.log(`Jornada ${matchId}`);
       const listItems = $('.list-resultado');
       listItems.each(function (idx, el) {
         if ($(el).text().indexOf('Deportivo') > 0) {
@@ -25,9 +24,9 @@ class Scrap {
             .trim();
         }
       });
-      //console.log(result);
+
       let gameMatch = result.split('                    ');
-      //console.log(gameMatch);
+
       if (gameMatch[0] == 'Deportivo') {
         match.riazor = true;
         match.rival = gameMatch[2];
@@ -38,49 +37,34 @@ class Scrap {
       match.result = gameMatch[1];
 
       let calendar = gameMatch[3].trim();
-      //console.log(calendar);
+
       match.day = `${calendar.substr(2, 5)}/${
         calendar.substr(5, 2) > 5 ? 2021 : 2022
       }`;
       match.hour = calendar.substr(8, 5);
 
-      // let day = match.day.split('/');
-      // match.datetime = `${day.reverse().join('/')} ${match.hour}`;
-      // const date = new Date(match.datetime);
-      // console.log(date, match.datetime);
-
-      //console.log(`El partido es el ${day} a las ${hour}`);
-
       let goals = gameMatch[1].split(' - ');
-      //console.log(goals);
 
       if (goals[0] == goals[1]) {
-        //console.log('Empató el Dépor');
         match.winner = 'Draw';
       } else if (
         (match.riazor && goals[0] > goals[1]) ||
         (!match.riazor && goals[1] > goals[0])
       ) {
-        //console.log('Ganó el Dépor!');
         match.winner = 'Dépor';
       } else if (gameMatch[1] == '-') {
-        //console.log('El partido no se jugó todavía');
         match.winner = 'Pending';
       } else if (gameMatch[1] == 'Aplaz.') {
-        //console.log('Aplazado');
         match.winner = 'Dépor';
         match.result = '3 - 0';
       } else {
-        //console.log('Perdió el Dépor');
         match.winner = match.rival;
       }
-      //console.log(match);
+
       matchSeries.push(match);
     }
     console.log('Scraped');
-    //console.log(matchSeries);
 
-    //TODO: Tenemos que insertar todos los partidos y, si existen, actualizarlos.
     return matchSeries;
   };
 }
